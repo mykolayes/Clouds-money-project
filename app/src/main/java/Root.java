1e9b14e8b81b2fdd8ee2d11f53ef8cfe8005dfaf
mykolayes <32031506+mykolayes@users.noreply.github.com>
2018-04-19 09:25:35 +0300

    Quotes, icons, transact upd

---
+++ app/src/main/java/Root.java
@@ -0,0 +1,31 @@
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import Modules.Wireframe.Wireframe;
+import Supports.Utils.TextReader;
+
+public class Root extends AppCompatActivity implements TextReader.ReadTextListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_root);
+        new TextReader(this, this);
+    }
+
+    @Override
+    public void onFinishReadingText() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Wireframe.getInstance().toHomeView(Root.this);
+            }
+        }, 2000);
+    }
+
+    @Override
+    public void onErrorReadingText() {
+    }
+}
